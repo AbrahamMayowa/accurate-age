2694f71c70e10f7d7238f847ab45a2a5a9821954
Mayowa Oluwasina <mayowaoluwasina@gmail.com>
2022-08-07 07:04:41 +0100

    request limit

--- src/age/age.controller.ts
+++ src/age/age.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { AgeService } from './age.service';
 import { GetAgeDto } from './dto/getAge.dto';
+import { ThrottlerGuard } from '@nestjs/throttler';
 
+@UseGuards(ThrottlerGuard)
 @Controller('/age')
 export class AgeController {
   constructor(private readonly ageService: AgeService) {}
--- src/age/age.module.ts
+++ src/age/age.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AgeController } from './age.controller';
 import { AgeService } from './age.service';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [],
+  imports: [
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 3,
+    }),
+  ],
   controllers: [AgeController],
   providers: [AgeService],
 })
